4b0d2155a95a293d3c699e319c8f8ac6ced395cf
empdo <emil.essung@live.se>
2023-01-09 21:31:04 +0100

    add eye rotation

--- Client/Assets/Scripts/PlayerScript.cs
+++ Client/Assets/Scripts/PlayerScript.cs
@@ -8,11 +8,17 @@ public class PlayerScript : MonoBehaviour
 
     public GameObject aimObject;
 
+    public GameObject leye;
+    public GameObject reye;
+
     public void PlayerRotation(float pitch, float yawn)
     {
         aimObject.transform.rotation = Quaternion.Euler(new Vector3(pitch, yawn, 0));
 
         transform.rotation = Quaternion.Euler(new Vector3(0, yawn, 0));
+
+        leye.transform.rotation = Quaternion.Euler(new Vector3(-pitch - 60f, 1, 0));
+        reye.transform.rotation = Quaternion.Euler(new Vector3(-pitch - 60f, 1, 0));
 
     }
 }
